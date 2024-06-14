adf54d261949526c37fac26eb24274d73de74ae9
IMperiumX <yusufadell.dev@gmail.com>
2024-06-14 04:07:38 +0300

    chore: Update static and media file paths in settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -122,13 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "blog" / "static"
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-MEDIA_ROOT = BASE_DIR / "uploads"
-MEDIA_URL = "/files/"
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = '/media/'
